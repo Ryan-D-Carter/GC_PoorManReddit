7b9862c613502d4ecd757c0e9911d72f36351798
Ryan <RyanCarterMI@gmail.com>
2021-07-23 15:40:12 -0400

    Initial commit

---
+++ GC_Lab27/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GC_Lab27.Models;
+
+namespace GC_Lab27.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AwwDAL _aww = new AwwDAL();
+
+       
+
+        public async Task<IActionResult> Index()
+        {
+            var aww = await _aww.GetAww();
+
+            return View(aww);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GC_Lab27/Models/AwwDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GC_Lab27.Models
+{
+    public class AwwDAL
+    {
+        private HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.reddit.com/");
+
+            return client;
+        }
+
+        public async Task<AwwObject> GetAww()
+        {
+            var client = GetClient();
+
+            var request = await client.GetAsync("r/aww/.json");
+            var json = await request.Content.ReadAsStringAsync();
+            var response = await request.Content.ReadAsAsync<AwwObject>();
+
+            return response;
+        }
+    }
+}
